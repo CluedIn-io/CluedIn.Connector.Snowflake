16fb9ff29858f65cae06536f70260cd0f3b71c38
Kieranties <Kieran@Kieranties.com>
2022-01-18 11:57:45 +0000

    fix: Ensure schedule and api limit set for provider
    AB#6966

--- src/Connector.Snowflake/SnowflakeConnectorProvider.cs
+++ src/Connector.Snowflake/SnowflakeConnectorProvider.cs
@@ -1,8 +1,11 @@
 using CluedIn.Connector.Common;
 using CluedIn.Connector.Common.Configurations;
 using CluedIn.Core;
+using CluedIn.Core.Crawling;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CluedIn.Connector.Snowflake
 {
@@ -14,5 +17,11 @@ namespace CluedIn.Connector.Snowflake
         }
 
         protected override IEnumerable<string> ProviderNameParts => new[] { CommonConfigurationNames.Host, CommonConfigurationNames.DatabaseName };
+
+        public override string Schedule(DateTimeOffset relativeDateTime, bool webHooksEnabled)
+            => $"{relativeDateTime.Minute} 0/23 * * *";
+
+        public override Task<CrawlLimit> GetRemainingApiAllowance(ExecutionContext context, CrawlJobData jobData, Guid organizationId, Guid userId, Guid providerDefinitionId)
+            => Task.FromResult(new CrawlLimit(-1, TimeSpan.Zero));
     }
 }
